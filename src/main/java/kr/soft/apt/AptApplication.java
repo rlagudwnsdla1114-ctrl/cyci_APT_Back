65e945aec6ef815ebebcc237c36ee4f2fd901d69
HHJ <hhj2025>
2026-01-05 13:42:48 +0900

    first

---
+++ src/main/java/kr/soft/apt/AptApplication.java
@@ -0,0 +1,13 @@
+package kr.soft.apt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/soft/apt/AptApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.soft.apt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
